406bb8c0525e62cc09e2ca60c9bc58b1974f8235
Hou-I Lin <hlin64@ucsc.edu>
2021-02-01 12:55:00 -0800

    pa3

---
+++ pa3/pa3-make-check.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/bash
+
+SRCDIR=https://raw.githubusercontent.com/houi-lin/w21/master/pa3
+
+if [ ! -e backup ]; then
+  mkdir backup
+fi
+
+cp *.c *.h Makefile backup   # copy all files of importance into backup
+
+
+echo ""
+echo ""
+
+rm -f *.o FindComponents
+
+echo "Press Enter To Continue with makefile check"
+read verbose
+
+make
+
+if [ ! -e FindComponents ] || [ ! -x FindComponents ]; then # exist and executable
+  echo ""
+  echo "Makefile probably doesn't correctly create Executable!!!"
+  echo ""
+else
+  echo ""
+  echo "Makefile probably correctly creates Executable!"
+  echo ""
+fi
+
+echo ""
+echo ""
+
+make clean
+
+if [ -e FindComponents ] || [ -e *.o ]; then
+  echo "WARNING: Makefile didn't successfully clean all files"
+fi
+
+rm -f *.o ModelListTest* ModelGraphTest* FindComponents garbage
+
